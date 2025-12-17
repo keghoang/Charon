288f1cc90e4b5511b1fcb9ead039050e88c80549
Kien Hoang <keg.hoang@gmail.com>
2025-12-17 10:45:05 -0800

    Remove obsolete prototype launch scripts

--- prototypes/launch_galt_clone_minimal.py
+++
@@ -1,9 +0,0 @@
-import sys, os, runpy
-
-repo = r"C:\\Users\\kien\\git\\Charon"
-if repo not in sys.path:
-    sys.path.insert(0, repo)
-
-galt_main = os.path.join(repo, "prototypes", "galt_clone", "galt", "main.py")
-runpy.run_path(galt_main, run_name="__main__")
-
